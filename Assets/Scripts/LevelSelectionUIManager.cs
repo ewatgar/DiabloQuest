bf8f235ac6e88f870d0bde117714697593cc2204
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-05-21 20:34:06 +0200

    Add warning messages level 2 and 3

--- Assets/Scripts/LevelSelectionUIManager.cs
+++ Assets/Scripts/LevelSelectionUIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,11 +12,13 @@ public class LevelSelectionUIManager : MonoBehaviour
     [SerializeField] private GameObject levelOneButton;
     [SerializeField] private GameObject levelTwoButton;
     [SerializeField] private GameObject levelThreeButton;
+    [SerializeField] private GameObject blockedLevelError;
     private GameData gameData;
 
 
     private void Start()
     {
+        blockedLevelError.SetActive(false);
         levelOneButton.GetComponent<Button>().onClick.AddListener(() => OnLevelSelectionButtonListener(1));
         levelTwoButton.GetComponent<Button>().onClick.AddListener(() => OnLevelSelectionButtonListener(2));
         levelThreeButton.GetComponent<Button>().onClick.AddListener(() => OnLevelSelectionButtonListener(3));
@@ -31,13 +34,24 @@ public class LevelSelectionUIManager : MonoBehaviour
                 break;
             case 2:
                 if (gameData.numCompletedLevels >= 1) SceneManager.LoadScene("BattleOneScene"); //TODO BattleTwoScene
-                else print("You need to finish level 1"); //TODO warning message level 2
+                else
+                {
+                    TextMeshProUGUI charName = blockedLevelError.GetComponent<TextMeshProUGUI>();
+                    charName.text = "Nivel 2 bloqueado";
+                    blockedLevelError.SetActive(true);
+                    print("You need to finish level 1");
+                }
                 break;
             case 3:
                 if (gameData.numCompletedLevels >= 2) SceneManager.LoadScene("BattleOneScene"); //TODO BattleThreeScene
-                else print("You need to finish level 2"); //TODO warning message level 3
+                else
+                {
+                    TextMeshProUGUI charName = blockedLevelError.GetComponent<TextMeshProUGUI>();
+                    charName.text = "Nivel 3 bloqueado";
+                    blockedLevelError.SetActive(true);
+                    print("You need to finish level 2");
+                }
                 break;
         }
-
     }
 }
