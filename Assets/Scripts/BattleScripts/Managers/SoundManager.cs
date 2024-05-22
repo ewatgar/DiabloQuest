dff4c9a9769d3ba38345740a2528311f2518935d
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-05-22 20:39:03 +0200

    Add Animation and Sound Managers

---
+++ Assets/Scripts/BattleScripts/Managers/SoundManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [Header("SFX")]
+    [SerializeField] private AudioSource audioSourceTest;
+    [Header("Music")]
+    [SerializeField] private AudioSource musicTest;
+}
