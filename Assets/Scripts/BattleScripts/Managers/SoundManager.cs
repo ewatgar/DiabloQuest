f89187ad2e2ce3a1b5aca532f42d228292140e0e
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-06-04 00:09:33 +0200

    decorate levels + wip SoundManager

--- Assets/Scripts/BattleScripts/Managers/SoundManager.cs
+++ Assets/Scripts/BattleScripts/Managers/SoundManager.cs
@@ -1,11 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    [Header("DEBUG")]
+    public AudioClip sfxDebug;
+    public AudioSource musicDebug;
+    [Header("Settings")]
+    public float volume;
     [Header("SFX")]
-    [SerializeField] private AudioSource audioSourceTest;
+    public AudioSource sfxUIButton;
+    public AudioSource sfxWin;
+    public AudioSource sfxLose;
+    public AudioSource sfxAttackMelee;
+    public AudioSource sfxAttackBow;
+    public AudioSource sfxAttackMagic;
+    public AudioSource sfxHurt;
+    public AudioSource sfxDying;
+    public AudioSource sfxPotion;
     [Header("Music")]
-    [SerializeField] private AudioSource musicTest;
+    public AudioSource musicMainMenu;
+    public AudioSource musicLevelSelection;
+    public AudioSource musicBattle;
+    public AudioSource musicBoosBattle;
+
+    private void Start()
+    {
+        AudioSource source = GetComponent<AudioSource>();
+        source.volume = volume;
+        source.clip = sfxDebug;
+        //source.Play();
+        //sfxDebug.GetComponent<AudioSource>().Play();
+    }
 }
