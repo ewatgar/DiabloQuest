18c08301a9eb6a4f221feea837106dc88848169d
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-06-04 22:48:22 +0200

    Update SoundManager.cs

--- Assets/Scripts/BattleScripts/Managers/SoundManager.cs
+++ Assets/Scripts/BattleScripts/Managers/SoundManager.cs
@@ -5,33 +5,118 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private static SoundManager _instance;
+    public static SoundManager Instance { get => _instance; }
+
     [Header("DEBUG")]
     public AudioClip sfxDebug;
-    public AudioSource musicDebug;
-    [Header("Settings")]
-    public float volume;
+    public AudioClip musicDebug;
     [Header("SFX")]
-    public AudioSource sfxUIButton;
-    public AudioSource sfxWin;
-    public AudioSource sfxLose;
-    public AudioSource sfxAttackMelee;
-    public AudioSource sfxAttackBow;
-    public AudioSource sfxAttackMagic;
-    public AudioSource sfxHurt;
-    public AudioSource sfxDying;
-    public AudioSource sfxPotion;
+    public AudioClip sfxUIButton;
+    public AudioClip sfxWin;
+    public AudioClip sfxLose;
+    public AudioClip sfxAttackMelee;
+    public AudioClip sfxAttackRange;
+    public AudioClip sfxKnockback;
+    public AudioClip sfxHeal;
+    public AudioClip sfxHurt;
+    public AudioClip sfxDying;
+    public AudioClip sfxPotion;
     [Header("Music")]
-    public AudioSource musicMainMenu;
-    public AudioSource musicLevelSelection;
-    public AudioSource musicBattle;
-    public AudioSource musicBoosBattle;
+    public AudioClip musicMainMenu;
+    public AudioClip musicLevelSelection;
+    public AudioClip musicBattle;
+    public AudioClip musicBossBattle;
 
-    private void Start()
+    private AudioSource _source;
+
+    private void Awake()
     {
-        AudioSource source = GetComponent<AudioSource>();
-        source.volume = volume;
-        source.clip = sfxDebug;
-        //source.Play();
-        //sfxDebug.GetComponent<AudioSource>().Play();
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+            _source = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void PlayAudio(AudioClip audio)
+    {
+        _source.clip = sfxDebug; //TODO change DEBUG
+        _source.Play();
+        print("AUDIO");
+    }
+
+    public void PlayUIButtonSFX()
+    {
+        PlayAudio(sfxUIButton);
+    }
+
+    public void PlayWinSFX()
+    {
+        PlayAudio(sfxWin);
+    }
+
+    public void PlayLoseSFX()
+    {
+        PlayAudio(sfxLose);
+    }
+
+    public void PlaySpellSFX(Spell spell)
+    {
+        if (spell.utilityType == UtilityType.Knockback) PlayAudio(sfxKnockback);
+        else if (spell.utilityType == UtilityType.Healing) PlayAudio(sfxHeal);
+        else if (spell.spellAreaType == SpellAreaType.Range) PlayAudio(sfxAttackRange);
+        else PlayAudio(sfxAttackMelee);
+    }
+
+    public void PlayHurtSFX()
+    {
+        PlayAudio(sfxHurt);
+    }
+
+    public void PlayDyingSFX()
+    {
+        PlayAudio(sfxDying);
+    }
+
+    public void PlayPotionSFX()
+    {
+        PlayAudio(sfxPotion);
+    }
+
+    public void PlayAttackRangeSFX()
+    {
+        PlayAudio(sfxAttackRange);
+    }
+
+    public void PlayMainMenuMusic()
+    {
+        PlayAudio(musicMainMenu);
+    }
+
+    public void PlayLevelSelectionMusic()
+    {
+        PlayAudio(musicLevelSelection);
+    }
+
+    public void PlayBattleMusic()
+    {
+        PlayAudio(musicBattle);
+    }
+
+    public void PlayBossBattleMusic()
+    {
+        PlayAudio(musicBossBattle);
+    }
+
+    public void StopAudio()
+    {
+        _source.Stop();
     }
 }
