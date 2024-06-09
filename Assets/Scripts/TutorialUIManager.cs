c693a08e3d1570b4f7033a51afe5148c586d3431
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-06-09 19:11:31 +0200

    add tutorial UI

---
+++ Assets/Scripts/TutorialUIManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialUIManager : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private GameObject tutorialPanel;
+    [SerializeField] private GameObject pageOne;
+    [SerializeField] private GameObject pageTwo;
+    [SerializeField] private Button firstPageNextButton;
+    [SerializeField] private Button secondPagePreviousButton;
+    [SerializeField] private Button secondPageFinishButton;
+
+    void Start()
+    {
+        tutorialPanel.SetActive(true);
+        pageOne.SetActive(true);
+        pageTwo.SetActive(false);
+        firstPageNextButton.onClick.AddListener(() => OnFirstPageNextButtonListener());
+        secondPagePreviousButton.onClick.AddListener(() => OnSecondPagePreviousButtonListener());
+        secondPageFinishButton.onClick.AddListener(() => OnSecondPageFinishButtonListener());
+    }
+
+    private void OnFirstPageNextButtonListener()
+    {
+        pageOne.SetActive(false);
+        pageTwo.SetActive(true);
+    }
+
+    private void OnSecondPagePreviousButtonListener()
+    {
+        pageOne.SetActive(true);
+        pageTwo.SetActive(false);
+    }
+
+    private void OnSecondPageFinishButtonListener()
+    {
+        tutorialPanel.SetActive(false);
+    }
+}
